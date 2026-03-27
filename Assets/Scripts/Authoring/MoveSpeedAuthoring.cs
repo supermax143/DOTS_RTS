6e6b9fadc9b26e7326d03a1bc410afbefbec0dfa
maxim.alekseev <supermax143@gmail.com>
2026-03-27 14:04:17 +0300

    added files

---
+++ Assets/Scripts/Authoring/MoveSpeedAuthoring.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MoveSpeedAuthoring : MonoBehaviour
+    {
+        public float Speed;
+        
+        public class Baker : Baker<MoveSpeedAuthoring>
+        {
+            public override void Bake(MoveSpeedAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, 
+                    new MoveSpeed
+                    {
+                        Speed = authoring.Speed
+                    } );
+            }
+        }
+        
+        
+    }
+    
+    public struct MoveSpeed : IComponentData
+    {
+        public float Speed;
+            
+    }
+}
